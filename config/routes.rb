d8ed91b7ba8f6d365bbca698ecf707b9afe955b9
Julien Lamouroux <lamourouxjulien+lewagon@icloud.com>
2024-05-31 08:51:14 +0200

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/new", to: "tasks#new", as: :new
+  get "tasks/:id", to: "tasks#show", as: :show
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update", as: :task
+  delete "tasks/:id", to: "tasks#delete", as: :delete
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
